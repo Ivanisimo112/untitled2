69d52e0070831b22052c648d687ccec48de90c49
Ivanisimo112 <vane4ka3334@gmail.com>
2021-06-03 15:34:26 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,35 @@
+package com.company;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(abc(128));
+    }
+
+    public static String abc(int n) {
+        int a = n;
+        int[] ints = new int[10];
+        while (n > 1) {
+            for (int i = 1; i < ints.length; i++) {
+                if (n == 1) {
+                    break;
+                }
+                if (n % (i + 1) == 0) {
+                    n = n / (i + 1);
+                    ints[i]++;
+                    i = 0;
+                }
+            }
+        }
+        String s = "n = " + a + " = ";
+        for (int i = 0; i < ints.length; i++) {
+            if (ints[i] > 1) {
+                s = s + "(" + (i + 1) + "**" + ints[i] + ")";
+            } else if (ints[i] == 1) {
+                s = s + "(" + (i + 1) + ")";
+            }
+        }
+        return s;
+    }
+}
