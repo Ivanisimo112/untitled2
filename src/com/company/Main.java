905462f3fb6f3bfc33fa16fa8e68c05a8a6efb83
Ivanisimo112 <vane4ka3334@gmail.com>
2021-06-04 01:34:04 +0300

    123

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,35 +1,60 @@
 package com.company;
 
-import org.w3c.dom.ls.LSOutput;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println(abc(128));
+        System.out.println(abc(31223808));
     }
 
     public static String abc(int n) {
-        int a = n;
-        int[] ints = new int[10];
+        int[] numbers = new int[0];
+        int[] power = new int[0];
         while (n > 1) {
-            for (int i = 1; i < ints.length; i++) {
-                if (n == 1) {
-                    break;
-                }
-                if (n % (i + 1 ) == 0) {
-                    n = n / (i + 1);
-                    ints[i]++;
-                    i = 0;
-                }
+            int smallerNumberByWhichToDivide = smallerNumberByWhichToDivide(n);
+            n = n / smallerNumberByWhichToDivide;
+            int cellNumber = cellNumber(numbers, smallerNumberByWhichToDivide);
+            if (cellNumber == -1) {
+                numbers = addItem(numbers, smallerNumberByWhichToDivide);
+                power = addItem(power, 1);
+            } else {
+                power[cellNumber]++;
             }
         }
-        String s = "n = " + a + " = ";
-        for (int i = 0; i < ints.length; i++) {
-            if (ints[i] > 1) {
-                s = s + "(" + (i + 1) + "**" + ints[i] + ")";
-            } else if (ints[i] == 1) {
-                s = s + "(" + (i + 1) + ")";
+
+        String s = "n = " + n + " = ";
+        for (int i = 0; i < numbers.length; i++) {
+            if (power[i] > 1) {
+                s = s + "(" + (numbers[i]) + "**" + power[i] + ")";
+            } else if (power[i] == 1) {
+                s = s + "(" + (numbers[i]) + ")";
             }
         }
         return s;
+    }
+
+    private static int[] addItem(int[] ints, int newNumb) {
+        int[] temp = new int[ints.length + 1];
+        for (int i = 0; i < ints.length; i++) {
+            temp[i] = ints[i];
+        }
+        temp[temp.length - 1] = newNumb;
+        return temp;
+    }
+
+    private static int smallerNumberByWhichToDivide(int n) {
+        for (int i = 2; i <= n; i++) {
+            if (n % i == 0) {
+                return i;
+            }
+        }
+        return n;
+    }
+
+    private static int cellNumber(int[] ints, int n) {
+        for (int i = 0; i < ints.length; i++) {
+            if (ints[i] == n) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
